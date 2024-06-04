2467023bd250308fe7093ff71a4f29d46be7564d
Sergey Igumnov <jkeevk@yandex.ru>
2024-06-04 15:42:52 +0300

    Employee corrections

--- Employee.sql
+++ Employee.sql
@@ -1,18 +1,11 @@
-CREATE TABLE IF NOT EXISTS superior (
-	superior_id SERIAL PRIMARY KEY,
-	name VARCHAR(30) UNIQUE NOT NULL
-);
-
-
 CREATE TABLE IF NOT EXISTS department (
 	department_id SERIAL PRIMARY KEY,
-	name VARCHAR(30) UNIQUE NOT NULL
+	department_name VARCHAR(30) NOT NULL
 );
-
 
 CREATE TABLE IF NOT EXISTS employee (
 	employee_id SERIAL PRIMARY KEY,
-	superior_id INTEGER NOT NULL REFERENCES superior(superior_id),
-	department_id INTEGER NOT NULL REFERENCES department(department_id),
-	name VARCHAR(30) NOT NULL
+	name VARCHAR(30) NOT NULL,
+	superior_id INTEGER REFERENCES employee(employee_id),
+	department_id INTEGER REFERENCES department(department_id)
 );
